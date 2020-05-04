db23960efa348981a208f077a93c15d01f5bb02d
TeresaHuang <sass1213teresa@gmail.com>
2020-05-04 12:24:21 -0400

    condensed

--- model_lane_multi.py
+++ model_lane_multi.py
@@ -10,18 +10,17 @@ from math import ceil
 import kornia
 
 
+###Unet: single view
 class UNet_features(nn.Module):
     def __init__(self, n_channels,Bilinear):
         super(UNet_features, self).__init__()
         self.inc = inconv(n_channels, 64)
         self.down1 = down(64, 128)
         self.down2 = down(128, 256)
-        self.down3 = down(256, 512)
-        self.down4 = down(512, 512)
-        self.up1 = up(1024, 256,bilinear=Bilinear)
-        self.up2 = up(512, 128, bilinear=Bilinear)
-        self.up3 = up(256, 64, bilinear=Bilinear)
-        self.up4 = up(128, 64, bilinear=Bilinear)
+        self.down3 = down(256, 256)
+        self.up1 = up(512, 128, bilinear=Bilinear)
+        self.up2 = up(256, 64, bilinear=Bilinear)
+        self.up3 = up(128, 64, bilinear=Bilinear)
         self.up_map = nn.UpsamplingBilinear2d((800,800))
         #self.outc = outconv(64, n_classes)
 
@@ -30,11 +29,9 @@ class UNet_features(nn.Module):
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
+        x = self.up1(x4, x3)
+        x = self.up2(x, x2)
+        x = self.up3(x, x1)
         #x = self.outc(x)
         return self.up_map(x)
 
