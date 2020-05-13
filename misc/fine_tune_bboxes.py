45d090bfcac314fdfec369ad33ad7e2e10832a62
Charles <charles@pop-os.localdomain>
2020-05-12 20:36:17 -0700

    add finetune code

---
+++ misc/fine_tune_bboxes.py
@@ -0,0 +1,182 @@
+import argparse, os
+from pathlib import Path
+import shutil
+
+import pickle
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import kornia
+
+from data_loading import get_loaders
+from loss import Loss, AverageMeter, save_checkpoint
+from model_bb import Yo2o, Yo4o
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='Flickr30k Training')
+    parser.add_argument('-batch_size', type=int, default=32,
+                        help='batch size')
+    parser.add_argument('-epoch', type=int, default=30,
+                        help='number of training epochs')
+    parser.add_argument('-lr', type=float, default=0.0001,
+                        help='initial learning rate')
+    parser.add_argument('-feature_size', type=int, default=20,
+                        help='image grid dimensions')
+    parser.add_argument('-num_bboxes', type=int, default=2,
+                        help='number of boxes per grid cell')
+    parser.add_argument('-lambda_coord', type=float, default=5.0,
+                        help='scalar for coordinate loss')
+    parser.add_argument('-lambda_noobj', type=float, default=0.5,
+                        help='scalar for confidence loss')
+    parser.add_argument('-thresh', type=float, default=0.5,
+                        help='prediction threshold')
+    parser.add_argument('--save', type=str, default=Path.cwd(),
+                        help='directory to save logs and models.')
+    parser.add_argument('--resume', default='', type=str,
+                        help='path to latest checkpoint (default: none)')
+    parser.add_argument('--results', default='results', type=str,
+                        help='name of results txt file')
+
+    return parser
+
+def main():
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # device = torch.device('cpu')
+    cudnn.benchmark = True
+
+    args = get_parser().parse_args()
+
+    learning_rate = args.lr
+    start_epoch = 0
+    max_val_ats = 0
+
+    trainloader, valloader = get_loaders('labeled', batch_size = args.batch_size, 
+            image_folder = '../data', annotation_file = '../data/annotation.csv')
+
+    model = Yo4o(args.feature_size, args.num_bboxes, device, load_pretrained = True).to(device)
+    loss_fxn = Loss(args.feature_size, args.num_bboxes, args.lambda_coord, args.lambda_noobj)
+    
+    params = list(list(model.lin1.parameters()) + list(model.lin2.parameters())+list(model.classifier.parameters()))
+    optimizer = torch.optim.Adam(params, lr=learning_rate)
+
+    if args.resume:
+        if os.path.isfile(args.resume):
+            checkpoint = torch.load(args.resume)
+            start_epoch = checkpoint['epoch']
+            max_val_ats= checkpoint['max_val_ats']
+            model.load_state_dict(checkpoint['model'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+        else: print("No checkpoint found at '{}'".format(args.resume))
+
+    if not args.resume:
+        file = open(f'{args.save}/{args.results}.txt','a')
+        file.write('Train_Loss,Val_ATS \n')
+        file.close()
+
+    train_loss = AverageMeter()
+
+    for epoch in range(start_epoch, 10):
+        start_epoch += 1
+        print('Epoch {}'.format(start_epoch))
+        model.train()
+        print('training...')
+        train_loss.reset()
+
+        for i, (sample, target, road_image) in enumerate(trainloader):
+            if i%10 ==  0:
+                print('[{}/{}] {}'.format(i,len(trainloader), train_loss.avg))
+            batch_size = len(sample)
+            img_batch = torch.stack(sample).to(device)
+            output = model(img_batch)
+            loss = loss_fxn(output, target, device)
+            train_loss.update(loss, batch_size)
+            model.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+
+        model.eval()
+        print('validating...')
+        val_ats = 0
+        for i, (sample, target, road_image) in enumerate(valloader):
+            with torch.no_grad():
+                img_batch = torch.stack(sample).to(device)
+                output = model(img_batch)
+                ats = loss_fxn.validate(output, target, conf_thresh = args.thresh)
+                val_ats += ats
+
+        val_ats /= 378
+        print('Validation ATS = {}, Learning Rate = {}'.format(val_ats, learning_rate))
+        is_best = max_val_ats < val_ats
+        max_val_ats = max(val_ats, max_val_ats)
+
+        if (epoch+1) % 5 == 0:
+            learning_rate /= 10
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = learning_rate
+
+        file = open(f'{args.save}/results.txt','a')
+        file.write('{},{}\n'.format(train_loss.avg,val_ats))
+        file.close()
+
+        save_checkpoint({
+            'epoch': epoch + 1, 'model': model.state_dict(),
+            'max_val_ats': max_val_ats, 'optimizer' : optimizer.state_dict(),
+        }, is_best, args.save)
+
+    learning_rate = args.lr/10
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    for epoch in range(start_epoch, args.epoch):
+        start_epoch += 1
+        print('Epoch {}'.format(start_epoch))
+        model.train()
+        print('training...')
+        train_loss.reset()
+
+        for i, (sample, target, road_image) in enumerate(trainloader):
+            if i%10 ==  0:
+                print('[{}/{}] {}'.format(i,len(trainloader), train_loss.avg))
+            batch_size = len(sample)
+            img_batch = torch.stack(sample).to(device)
+            output = model(img_batch)
+            loss = loss_fxn(output, target, device)
+            train_loss.update(loss, batch_size)
+            model.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+
+        model.eval()
+        print('validating...')
+        val_ats = 0
+        for i, (sample, target, road_image) in enumerate(valloader):
+            with torch.no_grad():
+                img_batch = torch.stack(sample).to(device)
+                output = model(img_batch)
+                ats = loss_fxn.validate(output, target, conf_thresh = args.thresh)
+                val_ats += ats
+
+        val_ats /= 378
+        print('Validation ATS = {}, Learning Rate = {}'.format(val_ats, learning_rate))
+        is_best = max_val_ats < val_ats
+        max_val_ats = max(val_ats, max_val_ats)
+
+        if (epoch+1) % 5 == 0:
+            learning_rate /= 10
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = learning_rate
+
+        file = open(f'{args.save}/results.txt','a')
+        file.write('{},{}\n'.format(train_loss.avg,val_ats))
+        file.close()
+
+        save_checkpoint({
+            'epoch': epoch + 1, 'model': model.state_dict(),
+            'max_val_ats': max_val_ats, 'optimizer' : optimizer.state_dict(),
+        }, is_best, args.save)
+
+if __name__ == '__main__': main()
